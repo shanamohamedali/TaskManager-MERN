e45e24ca61e7f38de739bb30f4ec6d596c3c26cd
shana <shaanamohamedali@gmail.com>
2025-08-31 13:54:29 +0400

    changed api to vercel domain

--- frontend/TodoApp/src/constants/constants.js
+++ frontend/TodoApp/src/constants/constants.js
@@ -1,1 +1,2 @@
-export const API_URL = "http://localhost:3005/api/todo-app/";
+export const API_URL = "https://task-manager-mern-coral.vercel.app/";
+//export const API_URL = "http://localhost:3005/api/todo-app/";
